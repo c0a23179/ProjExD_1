2d08e625365ed4959bcd3ecb0db7ced6c63eabe5
machida <c0a231791c@edu.teu.ac.jp>
2024-09-24 16:12:56 +0900

    練習５FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock(200)
+    clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)#練習２
@@ -22,7 +22,7 @@ def main():
 
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
